3e3f87213b87cce056a602cd53747addbd60f2ca
Spencer Gilson <spencer.gilson@gmail.com>
2025-03-23 13:34:56 -0700

    Fix wezterm keys on Macos

--- wezterm/dot-wezterm.lua
+++ wezterm/dot-wezterm.lua
@@ -1,13 +1,26 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 config = wezterm.config_builder()
 
-config.color_scheme = 'Monokai Pro (Gogh)'
+config.color_scheme = 'Google Dark (Gogh)'
 config.font_size = 14.0
 config.font = wezterm.font('JetBrains Mono')
 config.audible_bell = 'Disabled'
-config.use_dead_keys = false
-
 config.hide_tab_bar_if_only_one_tab = true
+config.use_dead_keys = false
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+config.keys = {
+	{ mods = "OPT", key = "LeftArrow", action = act.SendKey({ mods = "ALT", key = "b" }) },
+	{ mods = "OPT", key = "RightArrow", action = act.SendKey({ mods = "ALT", key = "f" }) },
+	{ mods = "CMD", key = "LeftArrow", action = act.SendKey({ mods = "CTRL", key = "a" }) },
+	{ mods = "CMD", key = "RightArrow", action = act.SendKey({ mods = "CTRL", key = "e" }) },
+	{ mods = "CMD", key = "Backspace", action = act.SendKey({ mods = "CTRL", key = "u" }) },
+	{ mods = "CMD|OPT", key = "LeftArrow", action = act.ActivateTabRelative(-1) },
+	{ mods = "CMD|OPT", key = "RightArrow", action = act.ActivateTabRelative(1) },
+	{ mods = "CMD|SHIFT", key = "LeftArrow", action = act.ActivateTabRelative(-1) },
+	{ mods = "CMD|SHIFT", key = "RightArrow", action = act.ActivateTabRelative(1) },
+}
 
 return config
